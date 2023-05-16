01e27546b2ddfda3d6d3852eaa00cddeedefd418
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-05-16 11:28:23 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = names => names.slice(0, 2);
+
+const returnLastTwoDrivers = names => names.slice(names.length - 2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => fare => fare * multiplier;
+
+const fareDoubler = fare => createFareMultiplier(2)(fare);
+
+const fareTripler = fare => createFareMultiplier(3)(fare);
+
+const selectDifferentDrivers = (drivers, driversSelection) => driversSelection(drivers);
